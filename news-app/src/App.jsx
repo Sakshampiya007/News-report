56e57f8332012181aba3378e8050850c4b05d014
Sakshampiya007 <piya.saksham007@gmail.com>
2024-07-27 14:59:09 +0545

    Added Navbar

---
+++ news-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './Components/Navbar';
+import Newsarea from './Components/Newsarea';
+
+
+const App = () => {
+  return (
+    <div>
+    <Navbar/>
+    <Newsarea/>
+    </div>
+  )
+}
+
+export default App
---
+++ news-app/src/Components/Newsarea.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Newsarea = () => {
+  return (
+    <h2 className="text-center">
+        <span className='badge bg-danger'>Latest News</span>
+    </h2>
+  )
+}
+
+export default Newsarea
